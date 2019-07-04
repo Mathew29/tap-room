2d35633055d59692b752c570a8accd4370bc98f1
Mathew Akre <mwakre29@gmail.com>
2019-07-04 16:06:55 -0700

    add the coldone img

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import coldone from '../assets/images/coldone.png';
 
 
 
@@ -18,12 +19,18 @@ export default function MainPage() {
           margin: 5px;
           box-shadow: 2px 2px 3px black;
         }
+
+        img {
+          width: 100px;
+          height: 100px;
+        }
       `}</style>
       <div className="container">
         <div className="center">
           <h1>Welcome To The Tap Room</h1>
 
-          <h4>Come And Enjoy Our Nice Beers</h4>
+          <h4>Come And Enjoy a Cold One</h4>
+          <img src={coldone} /> <br/>
 
           <Link to="/keglist"><button>View Kegs List</button></Link>
           <Link to="/addkeg"><button>Add New Keg</button></Link>
