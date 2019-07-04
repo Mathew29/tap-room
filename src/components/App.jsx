a0739d300171a8dfeafa96752b354491d977593e
Mathew Akre <mwakre29@gmail.com>
2019-07-04 11:31:46 -0700

    remove background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './NavBar';
 import MainPage from './MainPage';
 import KegList from './KegList';
 import AddKeg from './AddKeg';
-import background from '../assets/images/backgroundBeach.jpg';
+
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -13,9 +13,7 @@ function App() {
   return (
     <div>
       <style jsx>{`
-        background: url(${background});
-        background-size: 200%;
-        padding-bottom: 100%;
+
       `}</style>
       <NavBar/>
       <Switch>
