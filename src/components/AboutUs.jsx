444c82d562df9034839983de1e24c8ec9b6aaf46
Mathew Akre <mwakre29@gmail.com>
2019-07-04 16:18:31 -0700

    Create About us page

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AboutUs() {
+
+  return (
+    <div>
+      <h1>About Us</h1>
+      <h3>We are the bright minds behind the Tap Room!</h3>
+    </div>
+  )
+}
