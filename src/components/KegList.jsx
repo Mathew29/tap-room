fb3e73225336450801b20e999e8f11b1c10a3fd5
Mathew Akre <mwakre29@gmail.com>
2019-07-04 15:28:58 -0700

    add img to keg list

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import Keg from './Keg';
 import { Link } from 'react-router-dom';
+import coronaBeer from '../assets/images/Corona-Bottle.png'
 
 var masterKegList = [
   {
     name: 'Corona Extra',
+    img: coronaBeer,
     brand: 'Corona',
     price: '12',
     alcoholContent: '3'
   },
   {
     name: 'MoonShine',
+    img:
     brand: 'Basement Made',
     price: '20',
     alcoholContent: '25'
   },
   {
     name: 'DropTop',
+    img:
     brand: 'Beer',
     price: '10',
     alcoholContent: '7'
   },
   {
     name: 'Coors',
+    img:
     brand: 'Water',
     price: '5',
     alcoholContent: '1'
   },
   {
     name: 'Bud Light',
+    img:
     brand: 'Water',
     price: '5',
     alcoholContent: '1'
@@ -47,6 +53,7 @@ function KegList() {
         <hr />
         {masterKegList.map((keg, index) =>
           <Keg name={keg.name}
+            img={keg.img}
             brand={keg.brand}
             price={keg.price}
             alcoholContent={keg.alcoholContent}
