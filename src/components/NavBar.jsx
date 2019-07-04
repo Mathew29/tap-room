572cdabf242bf88251b71a3125760f12a569e463
Mathew Akre <mwakre29@gmail.com>
2019-07-04 11:41:45 -0700

    Style NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,11 +6,56 @@ function Header() {
   return (
     <div>
       <style jsx>{`
+      nav {
+        background-color: #CCC;
+        overflow: hidden;
+        padding: 1em;
+        border-bottom: 1px solid #000;
+      }
+      
+      nav	Link {
+          color: #000;
+      }
+      
+      nav	Link:visited {
+          color: #000;
+      }
+      
+      nav .navWide {
+        display: none;
+        margin: 0 auto;
+      }
+      
+      nav .navWide .wideDiv {
+        text-align: center;
+      }
+      
+      nav .navWide .wideDiv a {
+        text-decoration: none;
+        display: inline-block;
+        padding: 0 2em;
+      }
+
+      .hidden {
+        display: none;
+      }
+      
+      @media (min-width: 480px) {
+        
+        nav .navWide {
+          display: block;
+        }
+      
+      }
       `}</style>
 
       <header>
         <nav>
-          <Link to="/">Home</Link>
+          <div className="navWide">
+            <div className="wideDiv">
+              <Link to="/">Home</Link>
+            </div>
+          </div>
         </nav>
       </header>
 
