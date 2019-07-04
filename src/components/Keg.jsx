135705b7b45daa7b8233d1cf635cc2606c1967da
Mathew Akre <mwakre29@gmail.com>
2019-07-04 10:00:53 -0700

    add $ and % for price and alcoholContent

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -10,8 +10,8 @@ export default function Keg(props) {
       <div>
         <h2>{props.name}</h2>
         <h3>{props.brand}</h3>
-        <h3>{props.price}</h3>
-        <p><em>{props.alcoholContent}</em></p>
+        <h3>${props.price}</h3>
+        <p><em>{props.alcoholContent}%</em></p>
         <br/>
       </div>
       <hr/>
