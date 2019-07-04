022a9e5d7a78d9cd0eeb894704832be8e5868492
Mathew Akre <mwakre29@gmail.com>
2019-07-04 16:32:33 -0700

    add dicount in about us page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -8,10 +8,19 @@ export default function AboutUs() {
         .txt {
           text-align: center;
         }
+
+        .goobers {
+          font-size: 10px;
+        }
       `}</style>
       <div className="txt">
         <h1>About Us</h1>
         <h3>We are the bright minds behind the Tap Room!</h3>
+
+        <div className="goobers">
+          <p>For a 50% discount on ONE drink tell a staff member "I am a Goofy Goober"</p>
+          <p>This offer can only be used once for the first 50 customers.</p>
+        </div>
       </div>
     </div>
   )
