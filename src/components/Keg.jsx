51c98d72b7a3c6de0c425945d8bcafca7000ef86
Mathew Akre <mwakre29@gmail.com>
2019-07-04 08:59:52 -0700

    display KegList information using proptype

---
+++ src/components/Keg.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Keg(props) {
+  return (
+    <div>
+      <style jsx>{`
+
+      `}</style>
+      <div>
+        <h2>{props.name}</h2>
+        <h3>{props.brand}</h3>
+        <h3>${props.price}</h3>
+        <p><em>{props.alcoholContent}%</em></p>
+        <br/>
+      </div>
+      <hr/>
+    </div>
+  );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  alcoholContent: PropTypes.number.isRequired,
+};
