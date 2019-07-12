0bb413578fddeb27226805b70e2ff438004ce5c7
Mathew Akre <mwakre29@gmail.com>
2019-07-12 15:38:41 -0700

    add withRouter import and add edit button to route to edit page

--- src/components/KegDetails.jsx
+++ src/components/KegDetails.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
 function KegDetails(props) {
+
+  function handleEditRouteButton(e) {
+    e.preventDefault();
+    props.history.push('/keglist');
+  }
+
   return(
-    <div>
+    <div onSubmit={handleEditRouteButton}>
       <hr/>
       <h2>{props.selectedKeg.name}</h2>
       <h3>{props.selectedKeg.brand}</h3>
       <h3>${props.selectedKeg.price}</h3>
       <p><em>{props.selectedKeg.alcoholContent}%</em></p>
+      <button type="button">Edit</button>
       <hr/>
     </div>
   );
@@ -18,4 +26,4 @@ KegDetails.propTypes = {
   selectedKeg: PropTypes.object
 };
 
-export default KegDetails;
+export default withRouter(KegDetails);
