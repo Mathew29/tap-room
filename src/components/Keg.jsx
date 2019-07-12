34c96289f78ed6ce74d5d423a71ac8846a54e7cb
Mathew Akre <mwakre29@gmail.com>
2019-07-12 09:44:25 -0700

    add admin view and reg user view

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -2,40 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-export default function Keg(props) {
-  return (
-    <div>
-      <style jsx>{`
-        button {
-          box-shadow: 2px 2px 3px black;
-        }
-
-        img {
-          width 200px;
-          height 200px;
-          
-        }
-      `}</style>
-      <div>
-        <h2>{props.name}</h2>
-        <div className="beerImages">
-          <img src={props.img} />
-        </div>
-        <h3>{props.brand}</h3>
-        <h3>${props.price}</h3>
-        <p><em>{props.alcoholContent}%</em></p>
-        <Link to="/editkeg"><button>Edit Keg</button></Link>
-        <br/>
+function Keg(props) {
+  const editButton = <button>Edit Keg</button>;
+  const kegInfo = 
+  <div>
+    <h2>{props.name}</h2>
+    <h3>{props.brand}</h3>
+    <h3>${props.price}</h3>
+    <p><em>{props.alcoholContent}%</em></p>
+    <hr/>
+  </div>;
+  if(props.currentRouterPath === '/admin') {
+    return (
+      <div onClick={() => {props.onKegSelection(props.kegId);}}>
+        {kegInfo}
       </div>
-      <hr/>
-    </div>
-  );
+    );
+  } else {
+    return(
+      <div>
+        {kegInfo}
+      </div>
+    );
+  }
 }
 
 Keg.propTypes = {
   name: PropTypes.string.isRequired,
-  img: PropTypes.any.isRequired,
   brand: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
   alcoholContent: PropTypes.string.isRequired,
+  currentRouterPath: PropTypes.string,
+  onKegSelection: PropTypes.func,
+  kegId: PropTypes.string.isRequired
 };
+
+export default Keg;
