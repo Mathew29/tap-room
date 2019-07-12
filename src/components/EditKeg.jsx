9445ddebe6f6d9e68b95c91aa8ec0ae741e4ed94
Mathew Akre <mwakre29@gmail.com>
2019-07-12 15:37:56 -0700

    update button and remove Link tags

--- src/components/EditKeg.jsx
+++ src/components/EditKeg.jsx
@@ -24,7 +24,7 @@ export default function EditKeg() {
           <input type='text' id='brand' placeholder='BRAND' /><br />
           <input type='number' id='price' placeholder='PRICE' /><br />
           <input type='number' id='alcoholContent' placeholder='ALCOHOL CONTENT' /><br />
-          <Link to="/keglist"><button type='submit'>Edit Keg</button></Link>
+          <button type='submit'>Edit Keg</button>
         </form>
       </div>
     </div>
