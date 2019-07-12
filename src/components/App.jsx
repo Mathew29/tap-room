09550a8747bbaf382d8541b7c23cb59ddd0966ec
Mathew Akre <mwakre29@gmail.com>
2019-07-12 08:36:29 -0700

    make app component stateful

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,30 +5,49 @@ import KegList from './KegList';
 import AddKeg from './AddKeg';
 import EditKeg from './EditKeg';
 import AboutUs from './AboutUs';
-
+import { v4 } from 'uuid';
+import Error404 from './Error404';
 
 import { Switch, Route } from 'react-router-dom';
 
 
-function App() {
+class App extends React.Component {
 
-  return (
-    <div>
-      <style jsx>{`
-        background-color: #deb887;
-        background-size: 200%;
-        padding-bottom: 100%;
-      `}</style>
-      <NavBar/>
-      <Switch>
-        <Route exact path='/' component={MainPage}/>
-        <Route path='/keglist' component={KegList}/>
-        <Route path='/addkeg' component={AddKeg}/>
-        <Route path='/editkeg' component={EditKeg}/>
-        <Route path='/aboutus' component={AboutUs}/>>
-      </Switch>
-    </div>
-  );
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterKegList: {}
+    };
+    this.handleAddingNewKegToList = this.handleAddingNewKegToList.bind(this);
+  }
+
+  handleAddingNewKegToList(newKeg){
+    let newKegId = v4();
+    let newMasterKegList = Object.assign({}, this.state.masterKegList, { [newKegId]: newKeg
+    });
+    this.setState({masterKegList: newMasterKegList});
+  }
+
+  render() {
+    return (
+      <div>
+        <style jsx>{`
+          background-color: #deb887;
+          background-size: 200%;
+          padding-bottom: 100%;
+        `}</style>
+        <NavBar/>
+        <Switch>
+          <Route exact path='/' component={MainPage}/>
+          <Route path='/keglist' component={KegList}/>
+          <Route path='/addkeg' component={AddKeg}/>
+          <Route path='/editkeg' component={EditKeg}/>
+          <Route path='/aboutus' component={AboutUs}/>>
+        </Switch>
+      </div>
+    );
+  }
+  }
+
 
 export default App;
