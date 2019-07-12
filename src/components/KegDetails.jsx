2466666c9f099ab65d89dcd6b9dfd1f0da247cb8
Mathew Akre <mwakre29@gmail.com>
2019-07-12 15:55:03 -0700

    remove withRouter and change to Link

--- src/components/KegDetails.jsx
+++ src/components/KegDetails.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function KegDetails(props) {
 
-  function handleEditRouteButton(e) {
-    e.preventDefault();
-    props.history.push('/keglist');
-  }
-
   return(
-    <div onSubmit={handleEditRouteButton}>
+    <div>
       <hr/>
       <h2>{props.selectedKeg.name}</h2>
       <h3>{props.selectedKeg.brand}</h3>
       <h3>${props.selectedKeg.price}</h3>
       <p><em>{props.selectedKeg.alcoholContent}%</em></p>
-      <button type="button">Edit</button>
+      <Link to="/editkeg"><button>Edit</button></Link>
       <hr/>
     </div>
   );
@@ -26,4 +21,4 @@ KegDetails.propTypes = {
   selectedKeg: PropTypes.object
 };
 
-export default withRouter(KegDetails);
+export default KegDetails;
