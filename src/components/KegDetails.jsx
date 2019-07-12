3eb28a2d8684f6a206d79b3f76fafc2f42193a9c
Mathew Akre <mwakre29@gmail.com>
2019-07-12 13:06:29 -0700

    add props before selected keg and fix return typo

--- src/components/KegDetails.jsx
+++ src/components/KegDetails.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function KegDetails(props) {
-  reutrn(
+  return(
     <div>
       <hr/>
-      <h2>{selectedKeg.name}</h2>
-      <h3>{selectedKeg.brand}</h3>
-      <h3>${selectedKeg.price}</h3>
-      <p><em>{selectedKeg.alcoholContent}%</em></p>
+      <h2>{props.selectedKeg.name}</h2>
+      <h3>{props.selectedKeg.brand}</h3>
+      <h3>${props.selectedKeg.price}</h3>
+      <p><em>{props.selectedKeg.alcoholContent}%</em></p>
       <hr/>
     </div>
   );
